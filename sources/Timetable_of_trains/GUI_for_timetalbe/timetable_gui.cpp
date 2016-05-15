2f91434d8bffa697062ab3032e541a637f4b8b8c
Михаил Мальцев <mikle9997@mail.ru>
2016-05-15 20:03:25 +0300

    В догонку

--- sources/Timetable_of_trains/GUI_for_timetalbe/timetable_gui.cpp
+++ sources/Timetable_of_trains/GUI_for_timetalbe/timetable_gui.cpp
@@ -3,6 +3,7 @@
 TimetableGUI::TimetableGUI(int argc, char *argv[]) : argc(argc), argv(argv)
 {
     core = new CoreOfInfoAboutMetro;
+    core->loadInfoFromFile("metro_Saint-Petersburg_route_info.txt", "metro_Saint-Petersburg_station_info.txt");
 }
 
 int TimetableGUI::startGUI()
